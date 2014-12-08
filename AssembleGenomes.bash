71460e8b8802445224dd4a09e2e958f008277a67
hobrien <heath.obrien@gmail.com>
2014-12-08 12:45:19 +0000

    Separate out Assembly step from annotation and ortholog identification so they can be run independently

--- AssembleGenomes.bash
+++ AssembleGenomes.bash
@@ -1,52 +1,38 @@
 for species in $@
 do
+  #create all necessary folders
+  for folder in Assemblies Annotations exonerate_results \
+    exonerate_results/${species}_exonerate augustus_results augustus_results/${species}_split
+  do
+     if ! test -d $folder
+    then
+      mkdir $folder
+    fi
+  done
+  
   #Trim reads (Cla_fum and Tubaria only)
-  if test species = 'Cla_fum' or test species = 'Tubaria'
+  if test species = 'Cla_fum' || test species = 'Tubaria'
   then
     java -jar Trimmomatic-0.32.jar PE \
       -threads 24 \
       -phred33 \
       -trimlog Trimomatic/${species}_log.txt
-      Reads/${species}_1.fq Reads/${species}_2.fq \
-      Trimomatic/${species}_1_paired.fq.gz Trimomatic/${species}_1_unpaired.fq.gz \
-      Trimomatic/${species}_2_paired.fq.gz Trimomatic/${species}_2_unpaired.fq.gz \
+      Reads/${species}_1.fastq Reads/${species}_2.fastq \
+      Trimomatic/${species}_1_paired.fastq.gz Trimomatic/${species}_1_unpaired.fastq.gz \
+      Trimomatic/${species}_2_paired.fastq.gz Trimomatic/${species}_2_unpaired.fastq.gz \
       ILLUMINACLIP:Adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
-    mv Reads/${species}_1.fq Reads/${species}_untrimmed_1.fq
-    mv Reads/${species}_2.fq Reads/${species}_untrimmed_2.fq
-    gunzip -c Trimomatic/${species}_1_paired.fq.gz > Reads/${species}_1.fq
-    gunzip -c Trimomatic/${species}_2_paired.fq.gz > Reads/${species}_2.fq
+    mv Reads/${species}_1.fastq Reads/${species}_untrimmed_1.fastq
+    mv Reads/${species}_2.fastq Reads/${species}_untrimmed_2.fastq
+    gunzip -c Trimomatic/${species}_1_paired.fastq.gz > Reads/${species}_1.fastq
+    gunzip -c Trimomatic/${species}_2_paired.fastq.gz > Reads/${species}_2.fastq
   fi
   
-  #Assemble genomes
+  #Assemble genomes (this doesn't appear to be working for some reason)
   abyss-pe \
     np=24 \
     k=31 \
-    name=Assemblies/${species}.fa \
-    in='Reads/${species}_1.fq Reads/${species}_2.fq'
+    name=Assemblies/${species} \
+    in='Reads/${species}_1.fastq Reads/${species}_2.fastq'
 
-  #Annotate contigs
-  augustus --species=coprinus Assemblies/${species}.fa > Annotations/${species}.gff
-  getAnnoFasta.pl Annotations/${species}.gff
-
-  #Identify homologs
-  makeblastdb -dbtype prot -in Annotations/${species}.aa -parse_seqids
-  for query in `ls Copci1`
-  do
-    exonerate --model protein2genome \
-      --bestn 1  \
-      --verbose 0 \
-      --showalignment no \
-      --showvulgar no\
-      --ryo ">%ti %td\n%tcs\n" \
-      Copci1/$query Annotations/${species}.aa
-      > exonerate_results/${species}_exonerate/${species}_${query}
-    blastdbcmd -db Copci_all.fa \
-      -entry_batch <(blastp \
-        -query $query \
-        -db Annotations/${species}.aa \
-        -outfmt '6 sacc' \
-        -max_target_seqs 1) \
-        >augustus_results/${species}_augustis/${species}_${query}
-  done
 done
 
---
+++ ExtractHomologs.bash
@@ -0,0 +1,39 @@
+for species in $@
+do
+  #Annotate contigs
+  #
+  #  I was not able to install augustus on my system (OSX 10.9), so I am running it thru
+  #  virtual box.
+  #  Setup instructions are here: http://www.lecloud.net/post/52224625343/the-ultimate-setup-guide-ubuntu-13-04-in-virtualbox
+  #
+  #  The command here needs to be changed to the following:
+  #  ssh heath@ubuntu bash -c "'augustus --species=coprinus \
+  #  /mnt/Bioinformatics/Mushrooms/AgaricalesGenomics/Analyses/Assemblies/${species}.fa \
+  #  > /mnt/Bioinformatics/Mushrooms/AgaricalesGenomics/Analyses/Annotations/${species}.gff; \
+  #  getAnnoFasta.pl /mnt/Bioinformatics/Mushrooms/AgaricalesGenomics/Analyses/Annotations/${species}.gff'"
+  #
+  
+  augustus --species=coprinus Assemblies/${species}.fa > Annotations/${species}.gff
+  getAnnoFasta.pl Annotations/${species}.gff
+
+  #Identify homologs
+  makeblastdb -dbtype prot -in Annotations/${species}.aa -parse_seqids
+  for query in `ls Copci1`
+  do
+    exonerate --model protein2genome \
+      --bestn 1  \
+      --verbose 0 \
+      --showalignment no \
+      --showvulgar no\
+      --ryo ">%ti %td\n%tcs\n" \
+      Copci1/$query Assemblies/${species}.fa
+      > exonerate_results/${species}_exonerate/${species}_${query}
+    blastdbcmd -db Copci_all.fa \
+      -entry_batch <(blastp \
+        -query $query \
+        -db Annotations/${species}.aa \
+        -outfmt '6 sacc' \
+        -max_target_seqs 1) \
+        >augustus_results/${species}_split/${species}_${query}
+  done
+done
