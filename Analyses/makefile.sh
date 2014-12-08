f483ff3b581d6f9295a736ed13c14af862fd7961
hobrien <heath.obrien@gmail.com>
2014-12-08 15:43:49 +0000

    added the reference seqs to the prereqs for homolog extraction

--- Analyses/makefile.sh
+++ Analyses/makefile.sh
@@ -8,17 +8,16 @@ $(SPECIES)_scores.txt : augustus_results/$(SPECIES)_split exonerate_results/$(SP
 	bash ../RunNeedle.bash $(SPECIES)
 
 #Run blobology
-Blobology/$(SPECIES)_blob.txt Blobology/$(SPECIES)_order.png Blobology/$(SPECIES)_phylum.png Blobology/$(SPECIES)_species.png : Assemblies/$(SPECIES)-8.fa
-	bash ../Blobology.bash $(SPECIES)
+#Blobology/$(SPECIES)_blob.txt Blobology/$(SPECIES)_order.png Blobology/$(SPECIES)_phylum.png Blobology/$(SPECIES)_species.png : Assemblies/$(SPECIES)-8.fa
+#	bash ../Blobology.bash $(SPECIES)
 
 #Extract homologs
+augustus_results/$(SPECIES)_split exonerate_results/$(SPECIES)_exonerate: Assemblies/$(SPECIES).fa Copci1/*
+	bash ../ExtractHomologs.bash $(SPECIES)
+	touch $@
+
+#Assemble genomes
 Assemblies/$(SPECIES).fa: ../Reads/$(SPECIES)_1.fastq ../Reads/$(SPECIES)_2.fastq
 	bash ../AssembleGenomes.bash $(SPECIES)
 	touch $@
-	
 
-#Assemble genomes
-augustus_results/$(SPECIES)_split exonerate_results/$(SPECIES)_exonerate: Assemblies/$(SPECIES).fa
-	bash ../ExtractHomologs.bash $(SPECIES)
-	touch $@
-	
