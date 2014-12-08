91c006442eb1cce89b5e1ed0f5e849de6f12ff15
hobrien <heath.obrien@gmail.com>
2014-12-08 15:46:10 +0000

    added reference genes and raw reads as prereqs for scores file

--- makefile.sh
+++ makefile.sh
@@ -1,12 +1,13 @@
 #Run analyses necessary to assemble genomes and extract homologs
 
 SHELL=/bin/bash
-all :
+
+Mon_ror_scores.txt : Analyses/Copci1 Reads/Mon_ror_1.fastq Reads/Mon_ror_1.fastq
 	export SPECIES=Mon_ror; $(MAKE) -C Analyses
 
 #extract Copci1 sequences from each homolog set and write to indvidual files
-Copci1 : JGI_210genes_renamed_by_Copci1_gene/Copci1_*.fa
-	python ../ExtractSeqs.py JGI_210genes_renamed_by_Copci1_gene $@ $@
+Analyses/Copci1 : JGI_210genes_renamed_by_Copci1_gene/Copci1_*.fa
+	python ExtractSeqs.py JGI_210genes_renamed_by_Copci1_gene Copci1 $@
 	touch Copci1
 
 
