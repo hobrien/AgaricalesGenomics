25302505045e646b8e741e221ccc53a42cf66686
hobrien <heath.obrien@gmail.com>
2014-12-08 12:46:54 +0000

    add tests for existence of intermediary files so unnecessary steps don't need to be repeated
    this is like a poor man's MAKE, but I've been struggling to get make to do what I want

--- Blobology.bash
+++ Blobology.bash
@@ -8,7 +8,10 @@ do
   then
     fastaqual_select.pl -f Assemblies/${species}_l500.fa -s r -n 10000 >Blobology/${species}_10000.fa
   fi
-  blastn -task megablast -query Blobology/${species}_10000.fa -db nt -evalue 1e-5 -num_threads 8 -max_target_seqs 1 -outfmt '6 qseqid staxids' -out Blobology/${species}_10000.bl
+  if ! test -f Blobology/${species}_10000.bl
+  then
+    blastn -task megablast -query Blobology/${species}_10000.fa -db nt -evalue 1e-5 -num_threads 8 -max_target_seqs 1 -outfmt '6 qseqid staxids' -out Blobology/${species}_10000.bl
+  fi
   if ! test -f Assemblies/${species}.1.bt2 || \
      ! test -f Assemblies/${species}.2.bt2 || \
      ! test -f Assemblies/${species}.3.bt2 || \
@@ -18,27 +21,28 @@ do
   then
     bowtie2-build Assemblies/${species}_l500.fa Assemblies/${species}
   fi
-  if ! test -f Blobology/${species}.bam
+  if test -f Reads/${species}_1.fastq && test -f Reads/${species}_2.fastq && ! test -f Blobology/${species}.bam
   then
     bowtie2 -x Assemblies/${species} --very-fast-local -k 1 -t -p 12 --reorder --mm \
       -U <(shuffleSequences_fastx.pl 4 Reads/${species}_1.fastq Reads/${species}_2.fastq) \
       | samtools view -S -b -T Assemblies/${species}_l500.fa - > Blobology/${species}.bam
   fi
-  if ! test -f Blobology/${species}_blob.txt
+  if test -f Blobology/${species}.bam && ! test -f Blobology/${species}_blob.txt
   then
     gc_cov_annotate.pl --blasttaxid Blobology/${species}_10000.bl --assembly Assemblies/${species}_l500.fa --bam Blobology/${species}.bam --out Blobology/${species}_blob.txt --taxdump ~/Database/
   fi
-  if ! test -f Blobology/${species}_phylum.png
+  if test -f Blobology/${species}_blob.txt && ! test -f Blobology/${species}_phylum.png
   then
     makeblobplot.R Blobology/${species}_blob.txt 0.01 taxlevel_phylum Blobology/${species}_phylum.png
   fi
-  if ! test -f Blobology/${species}_order.png
+  if test -f Blobology/${species}_blob.txt && ! test -f Blobology/${species}_order.png
   then
     makeblobplot.R Blobology/${species}_blob.txt 0.01 taxlevel_order Blobology/${species}_order.png
   fi
-  if ! test -f Blobology/${species}_species.png
+  if test -f Blobology/${species}_blob.txt && ! test -f Blobology/${species}_species.png
   then
     makeblobplot.R Blobology/${species}_blob.txt 0.01 taxlevel_species Blobology/${species}_species.png
   fi
+  cp Blobology/${species}*.png ~/Google\ Drive/Blobology/
 done
 
