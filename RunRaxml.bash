1736ceb8a402c496771e6a389d687476e1c148c7
hobrien <heath.obrien@gmail.com>
2014-12-15 17:40:16 +0000

    commented out outgroup specification
    This turns out to be a pain because the analysis fails if any of the outgroup taxa are missing

--- RunRaxml.bash
+++ RunRaxml.bash
@@ -7,4 +7,4 @@ module load apps/RAxML-7.2.8-pthreads
 raxml-pthreads -T 4 -f a -m PROTGAMMAWAG -p 12345 -x 12345 -N autoMRE \
     -n $infile -w $HOME/Trees \
     -s ${HOME}/Alignments/${infile} \
-    -o -o Con_pu1,Ser_laS73,Ser_laS79
+    #-o Con_pu1,Ser_laS73,Ser_laS79
