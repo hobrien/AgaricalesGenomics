156bcad18f5fa7714ed846e8a6d9ca53f483d29c
hobrien <heath.obrien@gmail.com>
2014-12-18 13:44:05 +0000

    changed the name of outfiles so they don't clash with the non-partitioned analysis

--- RunRaxmlPartition.bash
+++ RunRaxmlPartition.bash
@@ -5,7 +5,7 @@
 
 module load apps/RAxML-7.2.8-pthreads
 raxml-pthreads -T 16 -f a -m PROTGAMMAWAG -p 12345 -x 12345 -N autoMRE \
-    -n $infile -w Trees \
+    -n ${infile}_partitioned -w Trees \
     -s Phylip/${infile} \
     -p partition.txt
 
