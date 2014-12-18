aaf87a00696b9fb378991baf9bbfd02f7cd96941
hobrien <heath.obrien@gmail.com>
2014-12-18 13:42:07 +0000

    changed the name of the assembly file to end in -scaffolds.fa

--- Analyses/makefile.sh
+++ Analyses/makefile.sh
@@ -1,23 +1,22 @@
 #Run analyses necessary to assemble genomes and extract homologs
 
 SHELL=/bin/bash
-	
 
 #Align homologs to references and select sequences with the highest alignment scores
 $(SPECIES)_scores.txt : augustus_results/$(SPECIES)_split exonerate_results/$(SPECIES)_exonerate/
 	bash ../RunNeedle.bash $(SPECIES)
 
 #Run blobology
-#Blobology/$(SPECIES)_blob.txt Blobology/$(SPECIES)_order.png Blobology/$(SPECIES)_phylum.png Blobology/$(SPECIES)_species.png : Assemblies/$(SPECIES)-8.fa
+#Blobology/$(SPECIES)_blob.txt Blobology/$(SPECIES)_order.png Blobology/$(SPECIES)_phylum.png Blobology/$(SPECIES)_species.png : Assemblies/$(SPECIES)-scaffolds.fa
 #	bash ../Blobology.bash $(SPECIES)
 
 #Extract homologs
-augustus_results/$(SPECIES)_split exonerate_results/$(SPECIES)_exonerate: Assemblies/$(SPECIES).fa Copci1/*
+augustus_results/$(SPECIES)_split exonerate_results/$(SPECIES)_exonerate: Assemblies/$(SPECIES)-scaffolds.fa Copci1/*
 	bash ../ExtractHomologs.bash $(SPECIES)
 	touch $@
 
 #Assemble genomes
-Assemblies/$(SPECIES).fa: ../Reads/$(SPECIES)_1.fastq ../Reads/$(SPECIES)_2.fastq
+Assemblies/$(SPECIES)-scaffolds.fa : ../Reads/$(SPECIES)_1.fastq ../Reads/$(SPECIES)_2.fastq
 	bash ../AssembleGenomes.bash $(SPECIES)
 	touch $@
 
