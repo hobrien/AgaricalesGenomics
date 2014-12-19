2a92a155ef45c2eb92fb4b54ef1ebffcf515cd10
hobrien <heath.obrien@gmail.com>
2014-12-19 16:10:40 +0000

    changed the dependancies for the blobology and all rules
    In theory, Analyses/Trees is dependent on Analyses/Alignments/Phylip/AllConcatinatedProt.phy
    so I replaced the latter with the former in the all rule
    
    Blobology is now dependent on the assemblies, which are in turn dependent on the reads, so it
    shouldn't be necessary for blobology to depend directly on the reads.
    
    The tricky part here is that "%-scaffolds.fa Reads/%_1.fastq" rule should apply whenever
    the reads exist, but the JGI download rules should substitute in other cases.
    
    I think it should be OK for the annotation step to be dependent on the Assembly step, because
    the AA translations will always be downloaded from JGI right after the assemblies, so they
    should always be more recent

--- makefile.sh
+++ makefile.sh
@@ -12,13 +12,34 @@ all : Analyses/Blobology/Bae_myo_phylum.png Analyses/Blobology/Cal_gam_phylum.pn
       Analyses/Blobology/Inocyb_phylum.png Analyses/Blobology/Mac_cum_phylum.png \
       Analyses/Blobology/Meg_pla_phylum.png Analyses/Blobology/Mon_ror_phylum.png \
       Analyses/Blobology/Mycena_phylum.png Analyses/Blobology/Pte_sub_phylum.png \
-      Analyses/Blobology/Tubaria_phylum.png Analyses/Alignments/Phylip/AllConcatinatedProt.phy
+      Analyses/Blobology/Tubaria_phylum.png Analyses/Trees
 
-Analyses/Blobology/%_phylum.png : Species/% Analyses/Assemblies/%-scaffolds.fa
+#Run RAxML on all alignments (this is done on the server)
+Analyses/Trees : Analyses/Alignments/Phylip/*
+	#export CLUSTER=$(CLUSTER) USER=$(USER); bash BuildTrees.bash
+	touch Analyses/Trees
+
+Analyses/Alignments/Phylip/AllConcatinatedProt.phy : Analyses/Scores/Mon_ror_scores.txt Analyses/Scores/Con_pu1_scores.txt \
+        Analyses/Scores/Ser_laS73_scores.txt Analyses/Scores/Ser_laS79_scores.txt Analyses/Scores/Bae_myo_scores.txt \
+        Analyses/Scores/Cal_gam_scores.txt Analyses/Scores/Cla_fum_scores.txt Analyses/Scores/Cli_gib_scores.txt \
+        Analyses/Scores/Cli_neb_scores.txt Analyses/Scores/Ent_cly_scores.txt Analyses/Scores/Gym_jun_scores.txt \
+        Analyses/Scores/Hyg_con_scores.txt Analyses/Scores/Inocyb_scores.txt Analyses/Scores/Mac_cum_scores.txt \
+        Analyses/Scores/Meg_pla_scores.txt Analyses/Scores/Mycena_scores.txt Analyses/Scores/Pte_sub_scores.txt \
+        Analyses/Scores/Tubaria_scores.txt Analyses/Scores/Guy_ne1_scores.txt
+	bash AlignSeqs.bash
+	python ConcatinateSeqs.py Analyses/Alignments/Gblocks Analyses/AllConcatinatedProt.fa > Analyses/partitions.txt
+	python ConvertAln.py -i Analyses/AllConcatinatedProt.fa -f phylip -o Analyses/Alignments/Phylip/AllConcatinatedProt.phy
+	touch Analyses/Alignments/Phylip
+
+Analyses/Scores/%_scores.txt : Species/% Analyses/Copci1 Analyses/Assemblies/%-scaffolds.fa
+	export SPECIES=$(<F); $(MAKE) -C Analyses
+
+Analyses/Blobology/%_phylum.png : Species/% Analyses/Assemblies/%-scaffolds.fa 
 	bash Blobology.bash $(<F)
 
-Analyses/Assemblies/%-scaffolds.fa Species/% Reads/%_1.fastq Reads/%_2.fastq
+Analyses/Assemblies/%-scaffolds.fa : Species/% Reads/%_1.fastq Reads/%_2.fastq
 	bash ../AssembleGenomes.bash $(<F)
+
 
 ##########################################################################################
 #Download JGI data for species not included in original gene family analysis
@@ -47,28 +68,12 @@ Analyses/Assemblies/Ser_la79-scaffolds.fa Analyses/Annotations/Ser_la79.aa : coo
        -b cookies -c cookies |zcat > Analyses/Annotations/Ser_la79.aa
 
 cookies :
-	curl https://signon.jgi.doe.gov/signon/create --data-ascii login=$(JGI_SIGNON)\&password=$(JGI_PASSWORD) -b cookies -c cookies > /dev/null
+	curl https://signon.jgi.doe.gov/signon/create --data-ascii login=$(JGI_SIGNON)\&password=$(JGI_PASSWORD) \
+       -b cookies -c cookies > /dev/null
 ##########################################################################################
 
-#Run RAxML on all alignments (this is done on the server)
-Analyses/Trees : Analyses/Alignments/Phylip/*
-	#export CLUSTER=$(CLUSTER) USER=$(USER); bash BuildTrees.bash
-	touch Analyses/Trees
 
-Analyses/Alignments/Phylip/AllConcatinatedProt.phy : Analyses/Scores/Mon_ror_scores.txt Analyses/Scores/Con_pu1_scores.txt \
-        Analyses/Scores/Ser_laS73_scores.txt Analyses/Scores/Ser_laS79_scores.txt Analyses/Scores/Bae_myo_scores.txt \
-        Analyses/Scores/Cal_gam_scores.txt Analyses/Scores/Cla_fum_scores.txt Analyses/Scores/Cli_gib_scores.txt \
-        Analyses/Scores/Cli_neb_scores.txt Analyses/Scores/Ent_cly_scores.txt Analyses/Scores/Gym_jun_scores.txt \
-        Analyses/Scores/Hyg_con_scores.txt Analyses/Scores/Inocyb_scores.txt Analyses/Scores/Mac_cum_scores.txt \
-        Analyses/Scores/Meg_pla_scores.txt Analyses/Scores/Mycena_scores.txt Analyses/Scores/Pte_sub_scores.txt \
-        Analyses/Scores/Tubaria_scores.txt Analyses/Scores/Guy_ne1_scores.txt
-	bash AlignSeqs.bash
-	python ConcatinateSeqs.py Analyses/Alignments/Gblocks Analyses/AllConcatinatedProt.fa > Analyses/partitions.txt
-	python ConvertAln.py -i Analyses/AllConcatinatedProt.fa -f phylip -o Analyses/Alignments/Phylip/AllConcatinatedProt.phy
-	touch Analyses/Alignments/Phylip
 
-Analyses/Scores/%_scores.txt : Species/% Analyses/Copci1 Reads/%_1.fastq Reads/%_2.fastq
-	export SPECIES=$(<F); $(MAKE) -C Analyses
 
 #extract Copci1 sequences from each homolog set and write to individual files
 Analyses/Copci1 : JGI_210genes_renamed_by_Copci1_gene/Copci1_*.fa
